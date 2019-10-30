bed0be823bc5da7363d544e8de717f6e1ac48922
Leonardo Santos <leonardo.nascimento@linx3.com>
2019-10-30 18:31:35 -0400

    feat: list all query parameters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+
+var metrics = {};
 
 app.get("/", function(req, res) {
   res.send("Prometheus Metrics Gateway");
+});
+
+app.get("/v1/counter/inc", function(req, res) {
+  for (var propName in req.query) {
+    if (req.query.hasOwnProperty(propName)) {
+      console.log(propName, req.query[propName]);
+    }
+  }
+  res.send("Counter incremented");
 });
 
 app.listen(3000, function() {
