f8c025c6fa72be5bfa480b49b9951895c5f6c02f
Leonardo Santos <leonardo.nascimento@linx3.com>
2019-10-31 11:41:30 -0400

    feat: increment metric

--- src/index.js
+++ src/index.js
@@ -7,14 +7,28 @@ app.get("/", function(req, res) {
   res.send("Prometheus Metrics Gateway");
 });
 
-app.get("/v1/counter/inc", function(req, res) {
+app.get("/v1/counter/:metric/inc", function(req, res) {
+  let key = {
+    metric: req.params.metric
+  };
   for (var propName in req.query) {
     if (req.query.hasOwnProperty(propName)) {
-      console.log(propName, req.query[propName]);
+      key[propName] = req.query[propName];
     }
   }
-  res.send("Counter incremented");
+  let keyString = JSON.stringify(key);
+  if (metrics[keyString] !== undefined) {
+    metrics[keyString]++;
+    if (metrics[keyString] < 0) {
+      metrics[keyString] = 1;
+    }
+  } else {
+    metrics[keyString] = 1;
+  }
+  res.send(metrics);
 });
+
+// TODO: reponse for prometheus
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
